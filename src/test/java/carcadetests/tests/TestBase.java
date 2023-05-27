3d633e0354d0493db7ba6a1e71094d5225643dfc
Konstantin Dubinin <kostos1995@mail.ru>
2023-05-27 21:42:03 +0300

    add allure

--- src/test/java/carcadetests/tests/TestBase.java
+++ src/test/java/carcadetests/tests/TestBase.java
@@ -1,7 +1,15 @@
 package carcadetests.tests;
 
+import carcadetests.helpers.Attach;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
 
 
 public class TestBase {
@@ -14,6 +22,29 @@ public class TestBase {
         Configuration.browser = browser[0];
         Configuration.browserVersion = browser[1];
 
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true, //чтобы отображалось в селенейде, что внутри просиходит
+                "enableVideo", true //чтобы писалась запись видео
+        ));
+
+        Configuration.browserCapabilities = capabilities;
+        Configuration.pageLoadStrategy = "eager";
+
+    }
+
+
+    @BeforeEach
+    void addListener() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+    @AfterEach
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
     }
 
 
