096eeca044add68c51d7b903071da86470876cf5
Konstantin Dubinin <kostos1995@mail.ru>
2023-05-27 21:00:54 +0300

    fix3

--- src/test/java/carcadetests/tests/TestBase.java
+++ src/test/java/carcadetests/tests/TestBase.java
@@ -1,2 +1,20 @@
-package carcadetests.tests;public class TestBase {
+package carcadetests.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+
+public class TestBase {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.remote = System.getProperty("remote");
+        Configuration.baseUrl = System.getProperty("baseUrl");
+        String[] browser = System.getProperty("browser").split(":");
+        Configuration.browser = browser[0];
+        Configuration.browserVersion = browser[1];
+
+    }
+
+
 }
