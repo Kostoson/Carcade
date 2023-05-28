cc8dc85f69dbf1879d18a27386835236e14a7235
Konstantin Dubinin <kostos1995@mail.ru>
2023-05-28 13:04:01 +0300

    add browser size

--- src/test/java/carcadetests/tests/TestBase.java
+++ src/test/java/carcadetests/tests/TestBase.java
@@ -18,6 +18,7 @@ public class TestBase {
     static void beforeAll() {
         Configuration.remote = System.getProperty("remote");
         Configuration.baseUrl = System.getProperty("baseUrl");
+        Configuration.browserSize = System.getProperty("browserSize");
         String[] browser = System.getProperty("browser").split(":");
         Configuration.browser = browser[0];
         Configuration.browserVersion = browser[1];
